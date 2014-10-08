01fea12e58f2c88c9e3e4c30558a36714e08bf6c
QX_Lai <laiqxure@gmail.com>
2014-10-08 14:48:48 -0400

    t3

--- ql516/assignment5.py
+++ ql516/assignment5.py
@@ -48,14 +48,60 @@ def mergeIntervals(int1, int2):
     s2=set(int2.data)
     if (s1-s2) == s1:
         return (int1.data[-1]+int2.data[-1])
-    else: 
-        return list(s1-s2)+int2.data
-    
-
+    else:
+        if int1.x<int2.x:
+            xx=str(int1.x)
+            if int1.l: ll='['
+            else: ll='('
+        else:
+            xx=str(int2.x)
+            if int2.l: ll='['
+            else: ll='('
         
+        if int1.y>int2.y:
+            yy=str(int1.y)
+            if int1.u :
+                uu=']'
+            else:
+                uu=')'
+        else:
+            yy=str(int2.y)
+            if int2.u :
+                uu=']'
+            else:
+                uu=')'
+        intvl= ll+xx+','+yy+uu
+        return interval(intvl)
+
+def isovlp(int1,int2):
+    s1=set(int1.data)
+    s2=set(int2.data)
+    if (s1-s2) == s1:  
+        return False
+    else:
+        return True
+    
+def mergeOverlapping(intlist):
+    itvstd = sorted(intlist,key=lambda intva: intva.data[0])
+    n=len(itvstd)
+    head =itvstd[0]
+    new = []
+    for i in range(1,n-1):
+        if isovlp(head,itvstd[i]):
+            head=mergeIntervals(head,itvstd[i])
+        else:
+            new.append(head)
+            head=itvstd[i]
+    new.append(head)
+    return new
+
 
 int1=interval("[12,18]")
 int2=interval("(17,20)")
 int_m=mergeIntervals(int1, int2)
 print int_m
-        
+
+intlist=[interval("[1,5]"),interval("[2,6)"),interval("(8,10]"),interval("[8,18]")]
+print intlist
+new = mergeOverlapping(intlist)
+print new
