9f2c3740f158ce09968db4334adccecaea249d00
Kayla Li <yl2612@nyu.edu>
2014-10-09 17:07:50 -0400

    commit

--- yl2612/my_package/assignment5.py
+++ yl2612/my_package/assignment5.py
@@ -21,7 +21,7 @@ class interval:
         else:
             self.u=self.upper_bound-1
         if self.l>self.u:
-            raise Exception("Error")
+            raise Exception("Invalid interval")
     def __repr__(self):
         return self.string
     
@@ -30,24 +30,30 @@ def mergeIntervals(int1, int2):
     if int1.u<int2.l-1 or int2.u+1< int1.l:
         raise Exception("Cannot merge")
     else:
-        if int1.l<int2.l:
-            merge_lower=int1.lower
-            merge_lower_bound=int1.lower_bound
+        if int1.l < int2.l:
+            merge_lower = int1.lower
+            merge_lower_bound = int1.lower_bound
+        elif int1.l == int2.l:
+            merge_lower = int1.lower if int1.lower_bound==int2.lower_bound else '('
+            merge_lower_bound=min(int1.lower_bound, int2.lower_bound)
         else:
-            merge_lower=int2.lower
-            merge_lower_bound=int2.lower_bound
-        if int1.u<int2.u:
-            merge_upper=int2.upper
-            merge_upper_bound=int2.upper_bound
+            merge_lower = int2.lower
+            merge_lower_bound = int2.lower_bound
+        if int1.u < int2.u:
+            merge_upper = int2.upper
+            merge_upper_bound = int2.upper_bound
+        elif int1.u == int2.u:
+            merge_upper = int1.upper if int1.upper_bound==int2.upper_bound else ')'
+            merge_upper_bound = max(int1.upper_bound, int2.upper_bound)
         else:
-            merge_upper=int1.upper
-            merge_upper_bound=int1.upper_bound
-        new_string=merge_lower+str(merge_lower_bound)+','+str(merge_upper_bound)+merge_upper
-        merge=interval(new_string)
+            merge_upper = int1.upper
+            merge_upper_bound = int1.upper_bound
+        new_string = merge_lower+str(merge_lower_bound)+','+str(merge_upper_bound)+merge_upper
+        merge = interval(new_string)
         return merge
 
 def mergeOverlapping(intlist):
-    intlist.sort(key=lambda x: x.lower_bound)
+    intlist.sort(key=lambda x: x.l)
     temp=intlist[0]
     li=[]
     for i in range(1,len(intlist)):
@@ -81,6 +87,10 @@ def main():
                 print 'Invalid interval'
                 continue
         
-
+#def test():
+    #int1=interval('[1,6]')
+    #int2=interval('(1,7)')
+    #print mergeIntervals(int1,int2)
 if __name__ == '__main__':
     main()
+    #test()
